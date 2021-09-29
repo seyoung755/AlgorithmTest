0da88b9662f2c6d94647b051ad494ffb497e72a8
SEYEONG KIM <seyoung7555@naver.com>
2021-09-30 01:01:26 +0900

    Quick sort 수정

--- Java Codingtest/src/QuickSort.java
+++ Java Codingtest/src/QuickSort.java
@@ -20,8 +20,7 @@ public class QuickSort {
         int pivot = arr[hi]; // 맨 오른쪽 값을 pivot으로 정함
         int left = lo; // 투 포인터 이용
         int right;
-        for (right = lo; right < hi; right++)
-        {
+        for (right = lo; right < hi; right++) {
             /*
             오른쪽 포인터를 이동하다가 pivot보다 작은 숫자가 등장하면
             왼쪽 포인터(pivot보다 큰 숫자임이 보장됨)의 값과 교환하고
@@ -40,15 +39,12 @@ public class QuickSort {
         // 즉, 퀵 정렬은 한번에 pivot 값만 정렬된다.
         // 정렬된 pivot을 기준으로 왼쪽, 오른쪽으로 나눠 분할정복 방식으로 정렬시켜 올라온다.
         swap(arr, hi, left);
-        System.out.println(Arrays.toString(arr));
 
         return left;
     }
 
     public static void rightPivotSort(int[] arr, int lo, int hi) {
 
-        System.out.println(lo);
-        System.out.println(hi);
         // 분할 정복 완료
         if (lo >= hi) {
             return;
@@ -57,13 +53,13 @@ public class QuickSort {
         int pivot = partition(arr, 0, hi);
 
         // pivot을 기준으로 좌우로 분할
-        rightPivotSort(arr, lo, pivot -1);
-        rightPivotSort(arr, pivot+1, hi);
+        rightPivotSort(arr, lo, pivot - 1);
+        rightPivotSort(arr, pivot + 1, hi);
     }
 
     public static void main(String[] args) {
-        System.out.println("Main function");
-        int[] arr = {3,7,2,4,1,9,6};
+        System.out.println("Quick sort");
+        int[] arr = {3, 7, 2, 4, 1, 9, 6};
         rightPivotSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
     }
