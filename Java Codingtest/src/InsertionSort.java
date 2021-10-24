d64dac91c618b0db8a1062af11dde98d9cf714c5
seyeong <seyoung7555@naver.com>
2021-10-24 15:03:41 +0900

    10/24 삽입정렬 구현
    삽입 정렬의 특징
            - 최선의 경우 O(n)의 시간 복잡도를 가진다. (모두 정렬된 싱태에서 한 요소만 추가되는 경우)
            - 최악의 경우에는 O(n^2)의 시간 복잡도를 가진다.
            - 제자리 정렬이다.
            - Stable sort이다.

--- Java Codingtest/src/InsertionSort.java
+++ Java Codingtest/src/InsertionSort.java
@@ -7,4 +7,35 @@
  */
 
 public class InsertionSort {
+
+    public static int[] insertionSort(int[] arr) {
+
+        for (int index = 1 ; index < arr.length ; index++) {
+            int temp = arr[index];
+            int prev = index - 1;
+
+            while (prev >= 0 && temp < arr[prev]) {
+                // temp보다 작으므로 들어갈 자리를 만들기 위해 오른쪽으로 한칸 밀린다.
+                arr[prev+1] = arr[prev];
+                prev--;
+
+            }
+            arr[prev+1] = temp;
+
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        /* 삽입 정렬의 특징
+        - 최선의 경우 O(n)의 시간 복잡도를 가진다. (모두 정렬된 싱태에서 한 요소만 추가되는 경우)
+        - 최악의 경우에는 O(n^2)의 시간 복잡도를 가진다.
+        - 제자리 정렬이다.
+        - Stable sort이다.
+         */
+        int[] arr = {6,3,10,-3,-10,0,17,-15};
+        int[] result = insertionSort(arr);
+        for (int i = 0; i < result.length ; i ++) System.out.println(result[i]);
+    }
 }
